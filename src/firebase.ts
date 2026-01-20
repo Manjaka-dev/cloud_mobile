5dd5fb23eca20be824db85d72b3b3806e04ca935
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2026-01-20 11:49:15 +0300

    add firebase initialization and authentication setup

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuiUJBK9_eEXVKjc89iUNn1gLHdrcqwQk",
+    authDomain: "fir-getting-started-6c8b0.firebaseapp.com",
+    projectId: "fir-getting-started-6c8b0",
+    storageBucket: "fir-getting-started-6c8b0.firebasestorage.app",
+    messagingSenderId: "1039491872784",
+    appId: "1:1039491872784:web:e4e3f10066bf40b624b295",
+    measurementId: "G-HDLX2L3XLW"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
